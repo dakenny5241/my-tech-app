e3a1ed3fff6c85419b816a153d53e7130e96a465
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-05-08 21:03:20 +0100

    config folder/file coded

---
+++ config/config.js
@@ -0,0 +1,16 @@
+// config.js
+module.exports = {
+    // Database configuration
+    database: {
+      host: 'localhost',
+      port: 3306,
+      username: 'username',
+      password: 'password',
+      database: 'database'
+    },
+  
+    // Session configuration
+    session: {
+      secret: 'keyboard cat'
+    }
+  };
